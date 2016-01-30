f90b824d09f41fd4328821489c80924a8ef417a4
Mikael Mukhsikaroyan <mikaelm@Mikaels-MacBook-Pro.local>
2016-01-30 08:12:42 -0800

    Created MemeEditorVC, implemented buttons

---
+++ MemeMe1.0/MemeEditorVC.swift
@@ -0,0 +1,55 @@
+//
+//  MemeEditorVC.swift
+//  MemeMe1.0
+//
+//  Created by Mikael Mukhsikaroyan on 1/30/16.
+//  Copyright © 2016 msquared. All rights reserved.
+//
+
+import UIKit
+
+class MemeEditorVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var chosenImage: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.chosenImage.image = image
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func pickImageFromAlbum(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func pickImageFromCamera(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+
+
+
+}
